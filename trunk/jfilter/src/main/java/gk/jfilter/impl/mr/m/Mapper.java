ee0b9f7928a23bb080f45a6bd779c12ca941853f
khankamranali@gmail.com <khankamranali@gmail.com@edee4a70-789c-9273-ca1f-09690c70844a>
2012-05-12 09:06:16 +0000

    MapReduce complete.

--- trunk/jfilter/src/main/java/gk/jfilter/impl/mr/m/Mapper.java
+++ trunk/jfilter/src/main/java/gk/jfilter/impl/mr/m/Mapper.java
@@ -1,93 +1,9 @@
 package gk.jfilter.impl.mr.m;
 
-import gk.jfilter.impl.filter.bean.Bean;
-import gk.jfilter.impl.filter.expression.ArrayFilterExpression;
-import gk.jfilter.impl.filter.expression.CollectionFilterExpression;
-
-import java.util.Arrays;
 import java.util.Collection;
 
-public class Mapper {
-	private Bean bean;
-	private Mapper mapper;
+public interface Mapper {
+	<O, T> void map(O object, Collection<T> list);
 
-	private Mapper(Bean bean, Mapper mapper) {
-		this.bean = bean;
-		this.mapper = mapper;
-	}
-
-	private Mapper(Bean bean) {
-		this.bean = bean;
-		this.mapper = null;
-	}
-
-	public <O, T> void map(O object, Collection<T> list) {
-		if (object == null) {
-			return;
-		}
-
-		Object po = bean.getValue(object);
-
-		if (po instanceof Collection) {
-			mapCollection((Collection<?>) po, list);
-		} else if (po.getClass().isArray()) {
-			mapArray((Object[]) po, list);
-		} else {
-			mapOne(po, list);
-		}
-	}
-	
-	private <O, T> void mapCollection(Collection<O> c, Collection<T> list) {
-		for (O o : c) {
-			mapOne(o, list);
-		}
-	}
-	
-	private <O, T> void mapArray(O[] os, Collection<T> list) {
-		for (O o : os) {
-			mapOne(o, list);
-		}
-	}
-	
-	private <O, T> void mapOne(O o, Collection<T> list) {
-		if (mapper != null) {
-			mapper.map(o, list);
-		} else {
-			list.add((T) o);
-		}
-	}
-
-	public Class<?> getType() {
-		if (mapper != null) {
-			return mapper.getType();
-		} else {
-			if (bean.getType().isArray()) {
-				return bean.getValueType();
-			} else if (Collection.class.isAssignableFrom(bean.getType())) {
-				return bean.getValueType();
-			} else {
-				return bean.getType();
-			}
-		}
-	}
-
-	public static Mapper parse(Bean bean, String property) {
-		String[] properties = property.split("\\.");
-		return parseDots(bean, properties);
-	}
-
-	private static Mapper parseDots(Bean bean, String[] properties) {
-		String nextProperty = properties[0];
-		Bean nextBean = bean.getProperty(nextProperty);
-
-		Mapper mapper;
-		if (properties.length > 1) {
-			// recursion
-			mapper = new Mapper(nextBean, parseDots(nextBean, Arrays.copyOfRange(properties, 1, properties.length)));
-		} else {
-			mapper = new Mapper(nextBean);
-		}
-		return mapper;
-	}
-
+	Class<?> getType();
 }
